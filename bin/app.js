e621fe71079a5c505efbdf3f39e80342b1334313
Theo Massard <massar_t@etna-alternance.net>
2017-09-24 19:23:44 +0200

    Prettify

--- bin/app.js
+++ bin/app.js
@@ -1,3 +1,4 @@
+// Author: massard-t
 const winston = require('winston');
 const process = require('process');
 const Bot = require('slackbots');
@@ -5,6 +6,7 @@ const Bot = require('slackbots');
 
 const token = process.env.API_KEY;
 const name = 'the_chill_one';
+const MENTIONNED = 'desktop_notification';
 
 const settings = {
   token,
@@ -12,23 +14,29 @@ const settings = {
 };
 
 
-const bot = new Bot(settings);
+const main = () => {
+  const bot = new Bot(settings);
 
-bot.on('start', () => {
-  winston.info(`${name} is up and running`);
-  // bot.postMessageToUser('theo', 'Hi Daddy!');
-});
+  bot.on('start', () => {
+    winston.info(`${name} is up and running`);
+    // bot.postMessageToUser('theo', 'Hi Daddy!');
+  });
 
-bot.on('message', (data) => {
-  switch (data.type) {
-    case 'desktop_notification':
-      // In this case, the bot is actually mentionned.
-      break;
-    case 'message':
-      // Process message if keyword matches ?
-      break;
-    default: break;
-  }
-  winston.info('Got some fresh new data c:');
-  // console.log(data);
-});
+  bot.on('message', (data) => {
+    switch (data.type) {
+      case MENTIONNED:
+        // In this case, the bot is actually mentionned.
+        break;
+      case 'message':
+        // Process message if keyword matches ?
+        break;
+      default: break;
+    }
+
+    winston.info('Got some fresh new data c:');
+    // console.log(data);
+  });
+};
+
+
+main();
